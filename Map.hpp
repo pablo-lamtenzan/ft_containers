4ca239b806f563e8071224670cdca61c67bae4a8
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2020-09-27 21:11:46 +0200

    map architecture ...

---
+++ Map.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Map.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plamtenz <plamtenz@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/27 21:05:28 by plamtenz          #+#    #+#             */
+/*   Updated: 2020/09/27 21:11:19 by plamtenz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "ReverseIterators.hpp"
+
+namespace ft {
+	
+	template<class Key, class T, class Compare, class Alloc>
+	class map
+	{
+		public:
+
+		/* typedefs */
+
+		/* cmp_class */
+
+		protected:
+
+		/* typedef node */
+
+		/* iterator */
+
+		/* reverse iterator */
+
+		public:
+
+		/* typedef iterators */
+
+		protected:
+
+		/* fill methods */
+
+		public:
+
+		/* member methods definition */
+
+		/* non members methods definition */
+	};
+}
